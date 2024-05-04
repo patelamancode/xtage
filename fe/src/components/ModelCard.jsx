33f18e2888ed8665b417b458b1a5eeaf7fa12f36
Aman <patelamancoder@gmail.com>
2024-05-04 23:21:31 +0530

    fix: fixing image in model card

--- fe/src/components/ModelCard.jsx
+++ fe/src/components/ModelCard.jsx
@@ -8,7 +8,7 @@ const ModelCard = (data) => {
     
       <div className="max-w-xs rounded-xl overflow-hidden shadow-lg cursor-pointer">
         <Link className='appearance-none no-underline' to={`/models/${model_name}`} >
-          <img className="w-full  h-[70%]" src={img} alt="Model" />
+          <img className="w-full h-[70%] object-fill object-center" src={img} alt="Model" />
         </Link>
         <div className="px-6 py-4">
             <div className="font-bold text-xl sm:text-sm md:text-lg lg:text-xl xl:text-xl mb-2">{model_name}</div>
